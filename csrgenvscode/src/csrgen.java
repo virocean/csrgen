d5df09e34f1d803db2603db5a8b0d3fe2fa287f6
David Wendl <david.wendl@continum.net>
2021-06-18 18:21:16 +0200

    added project for vscode and edited confirmingInput

--- csrgenvscode/src/csrgen.java
+++ csrgenvscode/src/csrgen.java
@@ -1,14 +1,15 @@
 package csrgen;
 import java.util.Scanner;
 
-public class Zertifikat {
+public class csrgen {
 /*
 1) Reading the Name of the Domain the Cert is being made for
 */
     String MAINFOLDER = "~/Zertifikate/";
     String USER = "DWE";
-    int TYPE;
     String DOMAIN;
+    int TYPE;
+    String EV;
     String PREFIX;
     String CN;
     String DIRECTORY;
@@ -23,18 +24,16 @@ public class Zertifikat {
 2) Choosing Cert Type and confirming
 */
     public void typeEingeben(){
-        System.out.prinf("%nNow let's choose the certificate type.%nThese are the Options:%n1.) AlphaSSL%n2.) Wildcard%n3.) SAN (Mit oder ohne EV)%n4.) LetsEncrypt%n5.) GeoTrust EV");
+        System.out.printf("%nNow let's choose the certificate type.%nThese are the Options:%n1.) AlphaSSL%n2.) Wildcard%n3.) SAN (Mit oder ohne EV)%n4.) LetsEncrypt%n5.) GeoTrust EV");
         TYPE = read.nextInt();
         while(true){
             switch(TYPE){
                 case 1:    //AlphaSSL
-                    continue;
                     continue;              
-                case 2:    //Wildcard
+                case 2:    //Wildcard                       
                     PREFIX=("wc.");
                     CN=("*.");
                     System.out.println("Extended Validation? yes/no: ");
-                    String EV;
                     EV = read.nextLine();
                     continue;
                 case 3:     //SAN
@@ -43,9 +42,11 @@ public class Zertifikat {
                 case 4:     //LetsEncrypt
                     System.out.println("LetsEncrypt Certificates can be installed over Service2:");
                     System.out.println("https://service2.continum.net/services/ssl-certificates");
-                    exit(0);
+                    System.exit(0);
+                    continue;
                 case 5:    //GeotrustEV                     
                     PREFIX=("www.");
+                    continue;
                 default:
                     System.out.println("%nNumber not between 1 and 5.%nTry again:%n");
             }    
@@ -53,7 +54,7 @@ public class Zertifikat {
     }
     public void confirmInput(String String) {
 
-        System.out.printf("%nHeres your inputs:%nCert Type: "+TYPE);
+        System.out.printf("%nHeres your inputs:%nDomain: "+DOMAIN+"%nCert Type: "+TYPE+"%n%");
         switch(TYPE){
             case 1:
                 System.out.println("AlphaSSL");
@@ -63,8 +64,14 @@ public class Zertifikat {
                 System.out.println("SAN");
             case 5:
                 System.out.println("Geotrust");
-            default:
-        } 
+        }
+        System.out.println("EV: ");
+            if (EV.equals("yes")){
+                System.out.printf("yes");
+            }else{
+                System.out.printf("no");
+            }
+        }
     }
 /*
 3) Creating dedicated Folder an executing the keygen-commands in it
