df4ba7e46904cca69b5a2cf2d5b12dbc945fb928
dborst <dborst@stud.hs-offenburg.de>
2021-06-19 12:02:42 +0200

    fixed switchcase

--- csrgen/src/csrgen/csrgen.java
+++ csrgen/src/csrgen/csrgen.java
@@ -32,59 +32,62 @@ public class csrgen {
 */
     public static void typeEingeben(){
         System.out.printf("%nNow let's choose the certificate type.%nThese are the Options:%n1.) AlphaSSL%n2.) Wildcard%n3.) SAN (Mit oder ohne EV)%n4.) LetsEncrypt%n5.) GeoTrust EV%n1, 2, 3, 4 or 5?: ");
-        boolean running = true;
+      //  boolean running = true;
         TYPE = read.nextInt();
-        while(running){
+      //  while(running){
             switch(TYPE){
                 case 1:     //AlphaSSL
                 case 5:     //GeotrustEV                     
-                    PREFIX=("www.");      
-                    continue;
+                    PREFIX=("www.");     
+                    EV = ("yes");
+                    break;
                 case 2:    //Wildcard                       
                     PREFIX=("wc.");
                     CN=("*.");
                     System.out.println("Extended Validation? yes/no: ");
                     EV = read.nextLine();
-                    continue;
+                    break;
                 case 3:     //SAN
                     PREFIX=("san.");
-                    continue;
+                    EV = ("yes");
+                    break;
                 case 4:     //LetsEncrypt
-                    System.out.println("LetsEncrypt Certificates can be installed over Service2:");
+                    System.out.printf("%nLetsEncrypt Certificates can be installed over Service2:");
                     System.out.println("https://service2.continum.net/services/ssl-certificates");
                     System.exit(0);
-                    continue;
+                    break;
                 default:
-                    System.out.println("%nNumber not between 1 and 5.%nTry again:%n");
+                    System.out.printf("%nNumber not between 1 and 5.%nTry again: %n"); 
+                    typeEingeben();        
             }
-            if  (TYPE >= 1 && TYPE <= 5){
-                confirmInput();
-                break;
+            System.out.printf("%n");
+       //     if  (TYPE >= 1 && TYPE <= 5){
+       //         confirmInput();
+       //         break;
                 
-            }
         }
-    }
+      //  }
+    
     public static void confirmInput() {
 
-        System.out.printf("%n%nHeres your inputs:%nDomain: "+DOMAIN+"%nCert Type: "+TYPE+"%n");
+        System.out.printf("%n%nHeres your inputs:%nDomain: "+DOMAIN+"%nCert Type: ");
         switch(TYPE){
             case 1:
                 System.out.println("AlphaSSL");
+                break;                
             case 2:
                 System.out.println("Wildcard");
+                break;                
             case 3:
                 System.out.println("SAN");
+                break;                
             case 5:
                 System.out.println("Geotrust");
+                break;                
         }
-        System.out.println("EV? yes/no: ");
-            if (EV.equals("yes")){
-                System.out.printf("yes%n");
-            }else{
-                System.out.printf("no%n");
-            }
-        }
-    }
+        System.out.printf("%nEV: "+EV+"%n");
+     }
+}
 /*
 3) Creating dedicated Folder an executing the keygen-commands in it
 */
