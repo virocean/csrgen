ab65c4024a933298658e56d1265dad2f299417ef
D-ctrl <davidpwendl@yahoo.de>
2021-06-20 16:41:43 +0200

    worked on makeDirectory

--- csrgen/src/csrgen/csrgen.java
+++ csrgen/src/csrgen/csrgen.java
@@ -1,28 +1,32 @@
 package csrgen;
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class csrgen {
  
         public static void main(String [] args){
         
-        domainEingeben();
-        typeEingeben();
+        inputDomain();
+        inputType();
         confirmInput();
+        makeDirectorys();
 }    
 /*
 1) Reading the Name of the Domain the Cert is being made for
 */
-    static String MAINFOLDER = "~/Zertifikate/";
-    static String USER = "DWE";
     static String DOMAIN;
     static int TYPE;
     static String EV;
-    static String PREFIX;
     static String CN;
+    static String PREFIX;
+    static String MAINFOLDER = "~/Zertifikate/";
     static String DIRECTORY;
+    static String USER = "DWE";
+
+
     static Scanner read = new Scanner(System.in);
 
-    public static void domainEingeben(){
+    public static void inputDomain(){
         System.out.print("Domain: ");
         DOMAIN = read.nextLine();
 
@@ -30,7 +34,7 @@ public class csrgen {
 /*
 2) Choosing Cert Type and confirming
 */
-    public static void typeEingeben(){
+    public static void inputType(){
         System.out.printf("%nNow let's choose the certificate type."
                 + "%nThese are the Options:"
                 + "%n1.) AlphaSSL"
@@ -38,46 +42,41 @@ public class csrgen {
                 + "%n3.) SAN%n4.) LetsEncrypt"
                 + "%n5.) GeoTrust EV"
                 + "%n1, 2, 3, 4 or 5?: ");
-      //  boolean running = true;
         TYPE = read.nextInt();
-      //  while(running){
             switch(TYPE){
-                case 1:     //AlphaSSL
-                case 5:     //GeotrustEV                     
-                    PREFIX=("www.");     
-                    EV = ("yes");
+                case 1: //AlphaSSL
+                    PREFIX=("www.");
+                    EV = ("No");
                     break;
-                case 2:    //Wildcard                       
+                case 2: //Wildcard                       
                     PREFIX=("wc.");
                     CN=("*.");
-                    System.out.println("Extended Validation? yes/no: ");
+                    System.out.print("Extended Validation? yes/no: ");
                     read.nextLine(); //catches the \n
                     EV = read.nextLine();
                     break;
-                case 3:     //SAN
+                case 3: //SAN
                     PREFIX=("san.");
                     System.out.print("Extended Validation? yes/no: ");
                     read.nextLine(); //catches the \n
                     EV = read.nextLine();
                     break;
-                case 4:     //LetsEncrypt
+                case 4: //LetsEncrypt
                     System.out.printf("%nLetsEncrypt Certificates can be installed over Service2:"
                     + "%nhttps://service2.continum.net/services/ssl-certificates");
                     System.exit(0);
                     break;
+                case 5: //GeotrustEV                     
+                    PREFIX=("www.");     
+                    EV = ("yes");
                 default:
                     System.out.printf("%nNumber not between 1 and 5.%n"
                             + "Try again: "
                             + "%n"); 
-                    typeEingeben();        
+                    inputType();        
             }
-            System.out.printf("%n");
-       //     if  (TYPE >= 1 && TYPE <= 5){
-       //         confirmInput();
-       //         break;
-                
+            System.out.printf("%n");       
         }
-      //  }
     
     public static void confirmInput() {
 
@@ -102,8 +101,29 @@ public class csrgen {
         }else{
             System.out.printf("no%n");
         }
-     }
-}
+    }
+
 /*
 3) Creating dedicated Folder an executing the keygen-commands in it
 */
+    public static void makeDirectorys() {
+        
+    //static String PREFIX;
+    //static String MAINFOLDER = "~/Zertifikate/";
+    //static String DIRECTORY;
+    //static String USER = "DWE";
+
+        
+        //date dir = new java.util.Date(System.currentTimeMillis());
+        String DIRECTORYNAME;
+        DIRECTORYNAME = (MAINFOLDER+"DATUM"+"-"+PREFIX+DOMAIN);
+        File DIRECTORY;
+        DIRECTORY = new File(DIRECTORYNAME);
+        
+        if (DIRECTORY.mkdir()){
+            System.out.println("Directory is created");
+        }else{
+            System.out.println("Directory cannot be created");
+        }
+    }   
+}
