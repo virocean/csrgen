cd6965c39097ab4da2a46b5143bfd004ff463acd
David Wendl <david.wendl@continum.net>
2022-03-08 17:42:35 +0100

    merge dev into main

--- csrgen.sh
+++ csrgen.sh
@@ -78,12 +78,9 @@ GeoTrust=5
     setPrefixAndCN(){
         case "${CERTIFICATE_TYPE}" in
             "${AlphaSSL}" | "${GeoTrust}" )
+                if [ "${CERTIFICATE_TYPE}" = "${GeoTrust}" ]; then EV="yes"; fi
                 #If it has a subdomain the prefix is not needed
-                    #case "${SUBDOMAIN}" in
-                    #    yes ) PREFIX="" ;;
-                    #    no ) PREFIX="www." CN="www." ;;
-                    #esac
-                if [ "${SUBDOMAIN}" == "yes" ]; then PREFIX=""; else PREFIX="www." CN="www."; fi
+                if [ "${SUBDOMAIN}" = "yes" ]; then PREFIX=""; else PREFIX="www." CN="www."; fi
                 ;;
             "${Wildcard}" )
                 PREFIX="wc." CN="*."
